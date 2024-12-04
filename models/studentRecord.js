829de7f287f5cc67a19452b2596cad3e370f0d35
mdnumans2024 <numanshorif2020@gmail.com>
2024-12-03 18:57:31 -0700

    Update

---
+++ models/studentRecord.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const attendanceSchema = mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['present', 'absent'],
+        required: true,
+    }
+});
+
+const studentRecordSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    attendance: {
+        type: [attendanceSchema],
+        default: [],
+    }
+
+});
+
+
+const StudentRecord = mongoose.model('StudentRecord', studentRecordSchema);
+
+module.exports = StudentRecord;
